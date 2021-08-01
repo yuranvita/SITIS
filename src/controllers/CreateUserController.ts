51523893cca92f93c211fa4518009d9a20173fb7
yuranvita <yuranxd1@gmail.com>
2021-08-01 11:49:08 -0400

    remove error

--- src/controllers/CreateUserController.ts
+++ src/controllers/CreateUserController.ts
@@ -1,5 +1,5 @@
 import { Request , Response}  from 'express';
-import { CreateUserService } from '../services/CreateUserService';
+import { CreateUserService } from '../Services/CreateUserService';
 
 class CreateUserController{
   async handle (req : Request , res : Response){
