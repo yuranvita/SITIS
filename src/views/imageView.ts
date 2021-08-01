fd5a4fca640500130d3bac8b791bd61cd0514c8c
yuranvita <yuranxd1@gmail.com>
2021-08-01 16:59:11 -0400

    Remove port

--- src/views/imageView.ts
+++ src/views/imageView.ts
@@ -6,7 +6,7 @@ export default {
  render(image : Image){
    return {
      id : image.id,
-     url : `https://sitis.herokuapp.com:3333/uploads/${image.path}`
+     url : `https://sitis.herokuapp.com/uploads/${image.path}`
    };
  },
  renderMany(images : Image[]){
