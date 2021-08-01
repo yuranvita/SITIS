a20c58396618464f53561329f8fa548da65711c6
yuranvita <yuranxd1@gmail.com>
2021-08-01 13:45:06 -0400

    fix error ip destination

--- src/views/imageView.ts
+++ src/views/imageView.ts
@@ -6,7 +6,7 @@ export default {
  render(image : Image){
    return {
      id : image.id,
-     url : `http:10.0.0.107:3333/uploads/${image.path}`
+     url : `http:10.0.0.111:3333/uploads/${image.path}`
    };
  },
  renderMany(images : Image[]){
